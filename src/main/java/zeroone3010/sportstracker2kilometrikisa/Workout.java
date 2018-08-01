cdfb9c2b90fa1aa61d1b03463c6d6ea5ad0ca903
Ville Saalo <ville.saalo@iki.fi>
2018-08-01 23:33:44 +0300

    Include the average speed in Workout's toString representation

--- src/main/java/zeroone3010/sportstracker2kilometrikisa/Workout.java
+++ src/main/java/zeroone3010/sportstracker2kilometrikisa/Workout.java
@@ -4,8 +4,11 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public final class Workout {
+  private static final float MPS_TO_KMPH = 3.6f;
+
   private final LocalDate date;
   private final Duration duration;
   private final BigDecimal totalDistanceInMeters;
@@ -40,8 +43,9 @@ public final class Workout {
 
   @Override
   public String toString() {
-    return String.format("[%s: %s km, %d:%02d:%02d]", date,
+    final float kmph = totalDistanceInMeters.floatValue() / duration.toSeconds() * MPS_TO_KMPH;
+    return String.format(Locale.ROOT, "[%s: %s km, %d:%02d:%02d; %.02f km/h]", date,
         getTotalDistanceInKilometers(),
-        duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart());
+        duration.toHours(), duration.toMinutesPart(), duration.toSecondsPart(), kmph);
   }
 }
--- src/test/java/zeroone3010/sportstracker2kilometrikisa/WorkoutTest.java
+++ src/test/java/zeroone3010/sportstracker2kilometrikisa/WorkoutTest.java
@@ -13,69 +13,69 @@ class WorkoutTest {
   void toStringWithZeroMinutes() {
     final Workout workout = new Workout(LocalDate.of(2018, 12, 31),
         Duration.ofSeconds(18L), BigDecimal.valueOf(0L), SportType.CYCLING);
-    assertEquals("[2018-12-31: 0.0 km, 0:00:18]", workout.toString());
+    assertEquals("[2018-12-31: 0.0 km, 0:00:18; 0.00 km/h]", workout.toString());
   }
 
   @Test
   void toStringWithZeroHours() {
     final Workout workout = new Workout(LocalDate.of(2018, 12, 31),
         Duration.ofMinutes(42L), BigDecimal.valueOf(0L), SportType.CYCLING);
-    assertEquals("[2018-12-31: 0.0 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-12-31: 0.0 km, 0:42:00; 0.00 km/h]", workout.toString());
   }
 
   @Test
   void toStringWithMoreThan24HoursAndSomeMinutesAndSeconds() {
     final Workout workout = new Workout(LocalDate.of(2018, 12, 31),
         Duration.ofSeconds(129723L), BigDecimal.valueOf(0L), SportType.CYCLING);
-    assertEquals("[2018-12-31: 0.0 km, 36:02:03]", workout.toString());
+    assertEquals("[2018-12-31: 0.0 km, 36:02:03; 0.00 km/h]", workout.toString());
   }
 
   @Test
   void toStringWithSingleDigitMonthAndDay() {
     final Workout workout = new Workout(LocalDate.of(2018, 1, 2),
         Duration.ofMinutes(42L), BigDecimal.valueOf(0L), SportType.CYCLING);
-    assertEquals("[2018-01-02: 0.0 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-01-02: 0.0 km, 0:42:00; 0.00 km/h]", workout.toString());
   }
 
   @Test
   void toStringWithOneMeter() {
     final Workout workout = new Workout(LocalDate.of(2018, 1, 2),
         Duration.ofMinutes(42L), BigDecimal.valueOf(1L), SportType.CYCLING);
-    assertEquals("[2018-01-02: 0.0 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-01-02: 0.0 km, 0:42:00; 0.00 km/h]", workout.toString());
   }
 
   @Test
   void toStringWithTenMeters() {
     final Workout workout = new Workout(LocalDate.of(2018, 1, 2),
         Duration.ofMinutes(42L), BigDecimal.valueOf(10L), SportType.CYCLING);
-    assertEquals("[2018-01-02: 0.0 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-01-02: 0.0 km, 0:42:00; 0.01 km/h]", workout.toString());
   }
 
   @Test
   void toStringWithFiftyMeters() {
     final Workout workout = new Workout(LocalDate.of(2018, 1, 2),
         Duration.ofMinutes(42L), BigDecimal.valueOf(50L), SportType.CYCLING);
-    assertEquals("[2018-01-02: 0.1 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-01-02: 0.1 km, 0:42:00; 0.07 km/h]", workout.toString());
   }
 
   @Test
   void toStringWith149Meters() {
     final Workout workout = new Workout(LocalDate.of(2018, 1, 2),
         Duration.ofMinutes(42L), BigDecimal.valueOf(149L), SportType.CYCLING);
-    assertEquals("[2018-01-02: 0.1 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-01-02: 0.1 km, 0:42:00; 0.21 km/h]", workout.toString());
   }
 
   @Test
   void toStringWith150Meters() {
     final Workout workout = new Workout(LocalDate.of(2018, 1, 2),
         Duration.ofMinutes(42L), BigDecimal.valueOf(150L), SportType.CYCLING);
-    assertEquals("[2018-01-02: 0.2 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-01-02: 0.2 km, 0:42:00; 0.21 km/h]", workout.toString());
   }
 
   @Test
   void toStringWith12345Meters() {
     final Workout workout = new Workout(LocalDate.of(2018, 1, 2),
         Duration.ofMinutes(42L), BigDecimal.valueOf(12345L), SportType.CYCLING);
-    assertEquals("[2018-01-02: 12.3 km, 0:42:00]", workout.toString());
+    assertEquals("[2018-01-02: 12.3 km, 0:42:00; 17.64 km/h]", workout.toString());
   }
 }
